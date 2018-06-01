d3928b1d7776bc119d05ab3f19b48a0fd62724bd
stringdoth <31211425+stringdoth@users.noreply.github.com>
2018-06-01 12:12:18 +0530

    Initial Commit

---
+++ TextRecogonition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TextRecogonition
+//
+//  Created by Bharath  Raj kumar on 01/06/18.
+//  Copyright © 2018 Pixl Ecosystems. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
